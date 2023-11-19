70b941aed32a452bc077eb9067d384a1c7a53dee
Marián Tarageľ <mariantaragel@gmail.com>
2023-11-19 18:46:23 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="FormatBench",
+    version="1.0",
+    description="Data format benchmark",
+    author="Marian Taragel",
+    author_email="xtarag01@stud.fit.vutbr.cz",
+)
